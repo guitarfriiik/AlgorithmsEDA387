51647dac9ae44b208a6b22903deaf789c087db95
Philip <philipkarlsson@me.com>
2014-10-10 14:22:54 +0200

    Quick fix

--- guiTree.py
+++ guiTree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
 """
@@ -17,6 +17,12 @@ from Tkinter import *
 def getCoord(x_offset,y_offset):
     return 400+x_offset,170+y_offset,460+x_offset,230+y_offset
 
+def getLineCoord(x_offset,y_offset):
+    return 430+x_offset,200+y_offset
+
+def getNumCoord(x_offset,y_offset):
+    return 428+x_offset,200+y_offset
+
 class Example(Frame):
   
     def __init__(self, parent):
@@ -30,22 +36,19 @@ class Example(Frame):
       
         self.parent.title("Networking Tree")        
         self.pack(fill=BOTH, expand=1)
+        # Init the coordinates
         self.coordinates=[(0,0), (125,-125), (170,0), (170,150), (0,150), (-175,50), (-350,100), (-350,200), (-150, -100), (-350, -100)]
-
         
-        # Replace by a loop using the coordinates
-        self.canvas.create_oval(getCoord(0,0), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(125,-125), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(170,0), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(170,150), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(0,150), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(-175,50), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(-350,100), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(-350,200), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(-150,-100), fill="white", outline="white")
-        self.canvas.create_oval(getCoord(-350,-100), fill="white", outline="white")
+        self.drawBase()
+        
+        # Setup the socket
+        # Read the first values
+        # Request the next values
 
-        #Attatch the routing and the numbers
+        # Draw text 
+        for i in range(0,10):
+            self.canvas.create_text(*getNumCoord(*self.coordinates[i]), anchor=W, font="Purisa",
+            text="1")
 
         b = Button(self.canvas, text="Step", width=30, command=self.update)
         b.pack(side=BOTTOM)
@@ -55,30 +58,49 @@ class Example(Frame):
         self.canvas.pack(fill=BOTH, expand=1)
 
     def update(self):
-        self.parent.title("Networking Tree 2")        
+        self.parent.title("Networking Tree")        
         self.pack(fill=BOTH, expand=1)
         
         self.canvas.delete("all") # Redraw the canvas
-
-        self.canvas.create_text(20, 30, anchor=W, font="Purisa",
-            text="Most relationships seem so transitory PUSSY")
-        self.canvas.create_text(20, 60, anchor=W, font="Purisa",
-            text="They're good but not the permanent one")
-        self.canvas.create_text(20, 130, anchor=W, font="Purisa",
-            text="Who doesn't long for someone to hold")
-        self.canvas.create_text(20, 160, anchor=W, font="Purisa",
-            text="Who knows how to love without being told")                   
-        self.canvas.create_text(20, 190, anchor=W, font="Purisa",
-            text="Somebody tell me why I'm on my own")            
-        self.canvas.create_text(20, 220, anchor=W, font="Purisa",
-            text="If there's a soulmate for everyone")
         
-        self.canvas.create_oval(100,100,200,200, fill="red", outline="red")
+        self.drawBase()
+
+        # Draw text 
+        for i in range(0,10):
+            self.canvas.create_text(*getNumCoord(*self.coordinates[i]), anchor=W, font="Purisa",
+            text="2")
 
         self.canvas.pack(fill=BOTH, expand=1)
 
 
+    def drawBase(self):
+        # Draw the connections
+        self.canvas.create_line(getLineCoord(*self.coordinates[0]), getLineCoord(*self.coordinates[1]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[0]), getLineCoord(*self.coordinates[2]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[0]), getLineCoord(*self.coordinates[4]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[0]), getLineCoord(*self.coordinates[5]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[0]), getLineCoord(*self.coordinates[8]), fill="white", width=2.0)
+
+        self.canvas.create_line(getLineCoord(*self.coordinates[1]), getLineCoord(*self.coordinates[0]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[1]), getLineCoord(*self.coordinates[2]), fill="white", width=2.0)
+
+        self.canvas.create_line(getLineCoord(*self.coordinates[3]), getLineCoord(*self.coordinates[2]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[3]), getLineCoord(*self.coordinates[4]), fill="white", width=2.0)
+
+        self.canvas.create_line(getLineCoord(*self.coordinates[5]), getLineCoord(*self.coordinates[6]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[5]), getLineCoord(*self.coordinates[4]), fill="white", width=2.0)
+        self.canvas.create_line(getLineCoord(*self.coordinates[3]), getLineCoord(*self.coordinates[4]), fill="white", width=2.0)
+
+        self.canvas.create_line(getLineCoord(*self.coordinates[6]), getLineCoord(*self.coordinates[7]), fill="white", width=2.0)
+
+        self.canvas.create_line(getLineCoord(*self.coordinates[8]), getLineCoord(*self.coordinates[9]), fill="white", width=2.0)
+
+        # Draw the circles
+        for i in self.coordinates:
+            self.canvas.create_oval(getCoord(*i), fill="white", outline="white")
+
 def main():
+    # start Main application
     root = Tk()
     ex = Example(root)
     root.geometry("820x450+300+300")
